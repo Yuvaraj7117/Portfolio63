623758661ece5fd388673edb5c9e4091d50be3a5
Yuvaraj7117 <danielguna494@gmail.com>
2024-08-19 00:00:52 +0530

    m

---
+++ src/data/constants.js
@@ -0,0 +1,303 @@
+export const Bio = {
+    name: "Yuvaraj",
+    roles: [
+      "Full Stack Developer",
+      "Programmer",
+    ],
+    description: "To kickstart my career as a software developer, utilizing my strong programming skills and eager to learn from experienced professionals. My goal is to contribute effectively to projects, innovate, and grow as a versatile developer while adding value to the organization",
+    github: "https://github.com/Yuvaraj7117",
+    resume: "https://drive.google.com/file/d/1OsM_PRIkbo9YhccJ0rmSfHKgDQz3_GTw/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/yuvarajp63/",
+    // twitter: "https://twitter.com/kjcreddy666",
+    // insta: "https://www.instagram.com/kjcreddy666/",
+    // facebook: "https://www.facebook.com/kjcreddy666/",
+};
+  
+export const skills = [
+    {
+      title: "Programming Languages",
+      skills: [
+        {
+          name: "C",
+          image: "C-logo.png",
+        },
+        {
+          name: "Java",
+          image: "java-logo.svg",
+        },
+      ],
+    },
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "HTML",
+          image: "html5-logo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "CSS3-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "bootstrap-logo.png",
+        },
+        {
+          name: "Tailwind Css",
+          image:
+            "tailwindcss.jfif",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "JavaScript-logo.png",
+        },
+        {
+          name: "React Js",
+          image:"ReactJs.png"
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "nodejs-logo.svg",
+        },
+        {
+          name: "Express Js",
+          image:"exx.jfif",
+        },
+        {
+          name: "Mongo DB",
+          image:"mDB.png",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:"Git.png",
+        },
+        {
+          name: "GitHub",
+          image:
+            "GitHub-logo.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "vscode-logo.png",
+        },
+      ],
+    },
+];
+  
+// export const experiences = [
+//     {
+//       id: 0,
+//       img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+//       role: "DevOps & Fullstack Engineering Intern",
+//       company: "Flipr Inovations Pvt. Ltd.",
+//       date: "Aug 2023 - July 2023",
+//       desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+//       skills: [
+//         "Docker",
+//         "Terraform",
+//         "AWS",
+//         "EC2",
+//         "Portainer",
+//         "Nginx",
+//         "JavaScript",
+//         "TypeScript",
+//         "Node Js",
+//         " Next Js",
+//       ],
+//       doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+//     },
+//     {
+//       id: 0,
+//       img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+//       role: "Frontend Engineer Intern",
+//       company: "Neurobit",
+//       date: "June 2023 - Present",
+//       desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+//       skills: [
+//         "ReactJS",
+//         "Redux",
+//         "NodeJs",
+//         "Material UI",
+//         "HTML",
+//         "CSS",
+//         "JavaScript",
+//       ],
+//       doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+//     },
+//     {
+//       id: 1,
+//       img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+//       role: "Fullstack Externship",
+//       company: "Flipr",
+//       date: "June 2023 - July 2023",
+//       desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+//       skills: [
+//         "ReactJS",
+//         "Redux",
+//         "NodeJs",
+//         "Material UI",
+//         "HTML",
+//         "CSS",
+//         "JavaScript",
+//         "Docker",
+//         "AWS",
+//         "MongoDB",
+//       ],
+//     },
+//     {
+//       id: 2,
+//       img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+//       role: "Open Source Contributor ",
+//       company: "GirlScript Summer of Code",
+//       date: "May 2023 - Present",
+//       desc: "Contributing to different open-source projects and learn from industry experts",
+//       doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+//     },
+//     {
+//       id: 3,
+//       img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
+//       role: "Android Developer",
+//       company: "DSC KIIT",
+//       date: "Nov2021 - Present",
+//       desc: "As an Android developer at the Google Developers Student Club (GDCS), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
+//       skills: [
+//         "Leadership",
+//         "Mobile Application Development",
+//         "Kotlin",
+//         "XML",
+//         "Figma",
+//       ],
+//     },
+//     {
+//       id: 4,
+//       img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
+//       role: "Android Developer Intern",
+//       company: "Rudraksha Welffare Foundation",
+//       date: "June 2021 - Oct 2021",
+//       desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
+//       skills: [
+//         "Android",
+//         "Java",
+//         "Kotlin",
+//         "XML",
+//         "Node Js",
+//         "Cloud Firestore",
+//         "Firebase",
+//         "Figma",
+//       ],
+//       doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+//     },
+// ];
+  
+export const education = [
+    {
+      id: 0,
+      img: "KIT-logo.jpg",
+      school: "Karpagam Institute Of Technology, Coimbatore",
+      date: "2021 - 2025",
+      grade: "7.93 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Information Technology at Karpagam Institute Of Technology, Coimbatore with a CGPA of 7.93. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+      degree: "Bachelor of Technology - BTech, Information Technology(IT)",
+    },
+    {
+      id: 1,
+      img: "Kadri.jfif",
+      school: "Kadri Mills Higher Secondary School, Oddarpalayam",
+      date: "2020 - 2021",
+      grade: "92.82 %",
+      desc: "I completed my Intermediate at Kadri Mills Higher Secondary School, Oddarpalayam where I studied Maths, Physics and Chemistry stream.",
+      degree: "Board Of Intermediate, TN MPC(Maths, Physics, Chemistry)",
+    },
+    {
+      id: 2,
+      img: "Kadri.jfif",
+      school: "Kadri Mills Higher Secondary School, Oddarpalayam",
+      date: "2018-2019",
+      grade: "88.8 %",
+      desc: "I completed my class 10 education at Kadri Mills Higher Secondary School, Oddarpalayam.",
+      degree: "Board Of Secondary Education, TN",
+    },
+];
+  
+export const projects = [
+    {
+      id: 9,
+      title: "Google User Interface",
+      description:
+        "Developed a custom Google UI using HTML5, CSS3, JavaScript,Tailwind CSS",
+      image:
+        "google2.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "ReactJs"
+      ],
+      category: "web app",
+      github: "https://github.com/Yuvaraj7117/google",
+      webapp: "#",
+    },
+
+    {
+      id: 9,
+      title: "Portfolio",
+      description:
+        "Explore my sleek portfolio, built with HTML5, CSS3, JavaScript, and React.js. Discover my skills and projects through seamless design and interactivity, optimized for any device.",
+      image:
+        "portfolio.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "ReactJS",
+      ],
+      category: "web app",
+      github: "https://github.com/Yuvaraj7117/Yuvafolio",
+      webapp: "#",
+    },
+
+    {
+      id: 9,
+      title: "BuyCart",
+      description:
+        "Implemented a mini-ecommerce web application using MERN stack",
+      image:
+        "Buycart.png",
+      tags: [
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "ReactJs",
+        "NodeJS",
+        "ExpressJS",
+        "MongoDB"
+      ],
+      category: "web app",
+      github: "https://github.com/Yuvaraj7117/BuyCart",
+      webapp: "#",
+    },
+
+ 
+];
+  
+export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+];
